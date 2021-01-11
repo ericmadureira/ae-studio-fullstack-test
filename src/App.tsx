7f8dc5e3bab5aeabeaa3ad06b365f630687a9aed
Eric Madureira <ericmadureira.uesc@gmail.com>
2021-01-10 22:34:11 -0300

    feat: set all routes
    Sets all remaining routes, including their domain.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import Layout from './components/Layout';
+import HomePage from './pages';
+import OldestBusinessReportPage from './pages/business/oldest-business-report';
+import MostLocationBusinessesReportPage from './pages/business/most-locations-businesses-report';
 
 function App() {
   return (
-    <>
-      <Layout>
-        <h1>Content goes here</h1>
-      </Layout>
-    </>
+    <Layout>
+      <Router>
+        <Switch>
+          {/* BUSINESS ROUTES */}
+          <Route path='/business/most-locations-businesses-report'>
+            <MostLocationBusinessesReportPage />
+          </Route>
+          <Route path='/business/oldest-business-report'>
+            <OldestBusinessReportPage />
+          </Route>
+          {/* HOME ROUTE */}
+          <Route path='/'>
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
+    </Layout>
   );
 }
 
