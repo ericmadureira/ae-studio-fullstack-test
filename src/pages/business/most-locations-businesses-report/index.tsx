51d626f735265b8bab88156dc4e9bc9159bc098a
Eric Madureira <ericmadureira.uesc@gmail.com>
2021-01-10 22:29:06 -0300

    feat: business pages
    Adds business domain pages.

---
+++ src/pages/business/most-locations-businesses-report/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MostLocationBusinessesReportPage = () => {
+  return (
+    <>
+      <h1>Most Locations Businesses Report</h1>
+      <Link to='/'>Back to Home</Link>
+    </>
+  );
+};
+
+export default MostLocationBusinessesReportPage;
---
+++ src/pages/business/oldest-business-report/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const OldestBusinessReportPage = () => {
+  return (
+    <>
+      <h1>Oldest Business Report</h1>
+      <Link to='/'>Back to Home</Link>
+    </>
+  );
+};
+
+export default OldestBusinessReportPage;
